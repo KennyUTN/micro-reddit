e180ffae7a35f188221e2825fa25ce0fc2bd50d1
KennyUTN <asisignacio@gmail.com>
2021-04-05 17:23:51 -0300

    models and Schema

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+belongs_to :user
+has_many :comment
+
+validates :url_link, presence: true, format: { with: %r{\A((https|http)://)?(www.)[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(:[0-9]{1,5})?(/.*)?\z}, message: 'must be a valid url_link' }
+end
